86622a03333ce1d316f5ca4b177ea780c2b8b5cf
Tanmay <tanmay.bpatil07@gmail.com>
2026-08-18 20:09:06 +0530

    Update monitoring engine configuration and notifications

--- services/monitoring-engine/notify.py
+++ services/monitoring-engine/notify.py
@@ -4,7 +4,7 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from config import config
+import config
 import logging
 
 logger = logging.getLogger(__name__)
